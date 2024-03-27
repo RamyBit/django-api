7b0cb62761166cf1f5ba2699bccd91041386db85
RamyBit <118202722+RamyBit@users.noreply.github.com>
2024-03-27 11:21:54 +0100

    api echo function created

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,20 @@
 from django.http import JsonResponse
+import json
 
 def api_home (request, *args, **kwargs):
-    return JsonResponse({"message": "Hello World from Django API"})
+    response = {}
+    params = request.GET
+    print("params: ", params)
+
+    if request.method == 'POST':
+        try:
+            request_data = json.loads(request.body)
+            print("request_data: ", request_data)
+        except json.JSONDecodeError:
+            return JsonResponse({"error": "Invalid JSON data in request body"}, status = 400)
+    
+    response['params'] = dict(params)
+    response['headers'] = dict(request.headers)
+    response['content_type'] = request.content_type
+    print("response: ", response)
+    return JsonResponse(response)
--- py_client/basic.py
+++ py_client/basic.py
@@ -2,7 +2,7 @@ import requests
 
 # Defining the URL and pyload data
 endpoint="http://localhost:11434"
-api="http://localhost:8000/api"
+api="http://localhost:8000/api/"
 url = "http://localhost:11434/api/generate"
 
 payload ={
@@ -16,17 +16,17 @@ payload ={
 get_response = requests.get(endpoint)
 print(get_response.text)
 # Sent POST request
-post_response = requests.post(url, json=payload)
+#post_response = requests.post(url, json=payload)
 
 # Get API request
-api_response = requests.get(api)
+api_response = requests.get(api, params={"no": 333}, json={"message": "Hi from client!"})
 
 # Print the API response
 print(api_response.json())
 
 # Check the response
-if post_response.status_code == 200:
-    print("Response: ")
-    print(post_response.json())
-else:
-    print("Error: ", post_response.status_code, post_response.text)
+# if post_response.status_code == 200:
+#     print("Response: ")
+#     print(post_response.json())
+# else:
+#     print("Error: ", post_response.status_code, post_response.text)
