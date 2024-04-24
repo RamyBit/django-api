88356a080019650904d25fd8e9519fb029893db7
RamyBit <118202722+RamyBit@users.noreply.github.com>
2024-04-24 13:36:32 +0200

    added: generics api view

--- backend/books/views.py
+++ backend/books/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Book
+from .serializers import BookSerializer
+
+class BookDetailAPIView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+book_detail_view = BookDetailAPIView.as_view()
